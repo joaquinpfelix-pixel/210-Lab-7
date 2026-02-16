9acce8dc29c9434af637d37b1940bbb185d23cfc
JoaquinF <joaquinpfelix@gmail.com>
2026-02-15 16:57:54 -0800

    Check 3

--- 210-Lab-7.cpp
+++ 210-Lab-7.cpp
@@ -14,6 +14,16 @@ int main()
 
     string* names = new string[ARRAY_SIZE];
 
+    *(names + 0) = "Janet";
+    *(names + 1) = "Jeffe";
+    *(names + 2) = "Jin";
+    *(names + 3) = "Joe";
+    *(names + 4) = "Junio";
+
+    cout << "Original array: ";
+    
+
+
 
 
 
@@ -23,10 +33,19 @@ string* reverseArray(string* arr, const int size)
 {
     for (int i = 0; i< size; i++)
     {
-        string temp - arr[i];
-        *(arr + i) = *(arr + size - i);
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + size - 1 - i);
         *(arr + size - 1 - i) = temp;
     }
 
     return arr;
+}
+
+void displayArray(string* arr, const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
 }
