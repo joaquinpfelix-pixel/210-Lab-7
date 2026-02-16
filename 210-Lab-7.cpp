cc3b5489f6d00833b16bcd179485efe4c387446a
JoaquinF <joaquinpfelix@gmail.com>
2026-02-15 16:46:20 -0800

    Check 2

--- 210-Lab-7.cpp
+++ 210-Lab-7.cpp
@@ -10,5 +10,23 @@ using namespace std;
 
 int main() 
 {
+    const int ARRAY_SIZE = 5;
 
+    string* names = new string[ARRAY_SIZE];
+
+
+
+
+}
+
+string* reverseArray(string* arr, const int size)
+{
+    for (int i = 0; i< size; i++)
+    {
+        string temp - arr[i];
+        *(arr + i) = *(arr + size - i);
+        *(arr + size - 1 - i) = temp;
+    }
+
+    return arr;
 }
